af138a1ba610e7b6c07a6d2b4d885673a4cf206f
Nick Sandy <69098674+ncsandy@users.noreply.github.com>
2022-07-24 18:42:25 -0400

    chapter 2

--- core/src/com/libgdx/bludbourne/BludBourne.java
+++ core/src/com/libgdx/bludbourne/BludBourne.java
@@ -1,31 +1,19 @@
 package com.libgdx.bludbourne;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.libgdx.bludbourne.screens.MainGameScreen;
 
-public class BludBourne extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class BludBourne extends Game {
+
+	public static final MainGameScreen _mainGameScreen = new MainGameScreen();
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		setScreen(_mainGameScreen);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	_mainGameScreen.dispose();
 	}
 }
--- desktop/src/com/libgdx/bludbourne/DesktopLauncher.java
+++ desktop/src/com/libgdx/bludbourne/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.libgdx.bludbourne;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.libgdx.bludbourne.BludBourne;
@@ -7,9 +9,15 @@ import com.libgdx.bludbourne.BludBourne;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		Lwjgl3ApplicationConfiguration config = new
+				Lwjgl3ApplicationConfiguration();
+
+		config.setTitle("Bludbourne");
+		config.setWindowedMode(800, 600);
 		config.setForegroundFPS(60);
-		config.setTitle("bludbourne");
-		new Lwjgl3Application(new BludBourne(), config);
+
+		Gdx.app = new Lwjgl3Application(new BludBourne(), config);
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
 	}
 }
